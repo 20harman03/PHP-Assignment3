18d6d69799f68e6c67a4ca0ccc6dfaff0d69a052
20harman03 <141785890+20harman03@users.noreply.github.com>
2023-08-12 04:31:29 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,6 @@
+    <!-- Common footer content -->
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> Your Company Name. All rights reserved.</p>
+    </footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Assignment Title</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <!-- Common header content -->
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
